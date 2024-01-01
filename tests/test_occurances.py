c23a4a237fda3bddac01e21194f5384a583233ad
Austin Flores <ctrl4ltdeleteme@gmail.com>
2024-01-02 00:28:26 +0100

    Fix

--- tests/test_occurances.py
+++ tests/test_occurances.py
@@ -1,5 +1,6 @@
 import json
 
-def test_occurances_returns_deploys(client):
+
+def test_request_example(client):
     response = client.get("/api/occurances/deploys")
-    assert [['2010-10-10T00:00:00', 1]] == json.loads(response.data)
+    assert [["2010-10-10T00:00:00", 1]] == json.loads(response.data)
